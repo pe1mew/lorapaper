da91ecd9526d66b00d9684cd44c977c805e56287
Sebastian Hammerl <github@omoco.de>
2019-12-22 22:28:21 +0100

    Added encoder and programming clip readme

---
+++ examples/WeatherForecastExample/ttn/encoder.js
@@ -0,0 +1,28 @@
+function Encoder(object, port) {
+  // Encode downlink messages sent as
+  // object to an array or buffer of bytes.
+  var bytes = [];
+
+  bytes[0] = object.temp || 0;
+  bytes[1] = object.humid || 0;
+  
+  bytes[3] = object.hours || 0;
+  bytes[4] = object.minutes || 0;
+  
+  if(object.rain === undefined) object.rain = [];
+  
+  bytes[6] = object.rain[0] || 0;
+  bytes[7] = object.rain[1] || 0;
+  bytes[8] = object.rain[2] || 0;
+  bytes[9] = object.rain[3] || 0;
+  bytes[10] = object.rain[4] || 0;
+  bytes[11] = object.rain[5] || 0;
+  bytes[12] = object.rain[6] || 0;
+  bytes[13] = object.rain[7] || 0;
+  bytes[14] = object.rain[8] || 0;
+  bytes[15] = object.rain[9] || 0;
+  bytes[16] = object.rain[10] || 0;
+  bytes[17] = object.rain[11] || 0;
+  
+  return bytes;
+}
---
+++ utils/programming clip/README.md
@@ -0,0 +1,1 @@
+After printing, heat up the end to bend the clip open. Insert the Pogo PINs and heat it up again to bend it back.
